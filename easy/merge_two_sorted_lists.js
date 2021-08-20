89b128e5178428467e1f781b6009e227538fc95d
YucenLyc <yucen.u@gmail.com>
2021-08-19 21:36:15 -0700

    the recursive solution to the problem

--- easy/merge_two_sorted_lists.js
+++ easy/merge_two_sorted_lists.js
@@ -1,87 +1,20 @@
-// function ListNode(val,next) {
-//   this.val = (val === undefined ? 0 : val)
-//   this.next = (next === undefined ? null : next)
-// }
-
+//recursive solution: 
 
 const mergeTwoLists = function(l1, l2) {
+  //if list1 is empty:
+  if (!l1) return l2;
 
-  if (!l1 && !l2) {
-    return null;
-  } else if (!l1 && l2){
-    return l2;
-  } else if (l1 && !l2) {
-    return l1;
-  } else if (l1.val < l2.val){
-    let currentVal = l1;
-    //console.log(currentVal)
-    currentVal.next = mergeTwoLists(l1.next, l2);
-    console.log(currentVal)
-    return currentVal;
-
-  } else {
-    let currentVal = l2;
-    currentVal.next = mergeTwoLists(l1, l2.next);
-    console.log(currentVal)
-    return currentVal;
-  }
+  //if list 2 is empty:
+  if (!l2) return l1;
   
-}
-l1 = [1, 3, 9]
-l2 = [1, 3, 10]
-mergeTwoLists(l1,l2)
-
- //check if both lists have numbers:
-
-//  if (!l1){
-//   return l2;
-// }
-// if (!l2) {
-//   return l1;
-// }
-// if(!l1 && !l2){
-//   return null;
-// }
-
-// //compare two lists:
-// let result = [];
-// for (let i = 0; i < l1.length; i++){
-//  for (let j = 0; j < l2.length; j++) {
-//    if (l1[i] < l2[j]){
-//      //console.log(l1[i], l2[j])
-//      result.push(l1[i]);
-//      //console.log("this is the result inside the first if:", result);
-//      i++;
-//      if (l1[i] > l2[j]){
-//        result.push(l2[j]);
-//      }
-//    } else if  (l1[i] === l2[j]){
-//     //console.log(l1[i], l2[j])
-//      result.push(l1[i], l2[j])
-//      i++;
-//     } else {
-//      result.push(l2[j]);
-//      console.log("this is the case l2 is bigger:", result);
-//      j++;
-//    }
-//  }
-// }
-// console.log(result);
-
-//compare two lists:
-let result = [];
-if (l1.val <= l2.val){
-  console.log
-  let currentEle = l1;
-  currentEle.next = mergeTwoLists(l1.next, l2);
-  //console.log("this is the console inside the first if:", currentEle);
-  return currentEle;
-} else {
-  console.log(l1.val); 
-  console.log(l2.val);
-  let currentEle = l2;
-  currentEle.next = mergeTwoLists(l2, l1.next);
-  //console.log("this is the console inside the second if:",currentEle);
-  return currentEle;
-}
-
+  //when the value in list1 is smaller:
+  if (l1.val < l2.val) {
+      l1.next = mergeTwoLists(l1.next, l2);
+      //console.log(l1)
+      return l1;
+  }
+  //otherwise:
+  l2.next = mergeTwoLists(l2.next, l1);
+  //console.log(l2)
+  return l2;
+};
