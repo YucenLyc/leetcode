9dbff70c0937777769d7dd5abdaacceaf89a2d21
YucenLyc <yucen.u@gmail.com>
2021-08-25 14:42:26 -0700

    slove plus one problem

---
+++ easy/plusOne.js
@@ -0,0 +1,20 @@
+//  Given a non-empty array of decimal digits representing a non-negative integer, increment one to the integer.
+
+const plusOne = function(digits) {
+  let i = digits.length - 1;
+  while (digits[i] === 9) {
+      digits[i] = 0;
+      i--;
+  }
+  if (i < 0) {
+      digits[0] = 1;
+      digits.push(0);
+  } else {
+      digits[i] += 1;
+  }
+  console.log(digits);
+  return digits;
+};
+
+digits = [9];
+plusOne(digits);
