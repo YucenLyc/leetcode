d3d9e629da4c5969fb2d94316762b2b4d07a271a
YucenLyc <yucen.u@gmail.com>
2021-09-11 15:58:50 -0700

    solve remove Nth node from the end of a linked list problem with three pointers

--- medium/remove.nth.node.from.end.of.list.js
+++ medium/remove.nth.node.from.end.of.list.js
@@ -1,1 +1,29 @@
 // Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+const removeNthFromEnd = function(head, n) {
+  if (head === null) {
+      return null;
+  };
+  // creating a dummy node to take care of the case when the first node is the node we want to delete:
+  let dummyHead = new ListNode(-1);
+  dummyHead.next = head;
+  let count = 0;
+  let tail = head;
+
+  while (count < n) {
+      tail = tail.next;
+      count++;
+  }
+  let removeNode = head;
+  let prevNode = dummyHead;
+
+  while (tail) {
+      tail = tail.next;
+      removeNode = removeNode.next;
+      preNode = preNode.next;
+  }
+  // when tail gets to null:
+  // delete the removeNode 
+  prevNode.next = removeNode.next;
+  return dummyHead.next;
+};
