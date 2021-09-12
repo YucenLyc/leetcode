f8ed01870dde10ac59e6075373de48999b66156e
YucenLyc <yucen.u@gmail.com>
2021-09-12 14:08:59 -0700

    solve valid palindrome problem with string reverse

---
+++ easy/validPalindrome.js
@@ -0,0 +1,14 @@
+// Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+const isPalindrome = function(s) {
+  s = s.toLowerCase().replace(/[^0-9a-z]/g, "");
+  let k = s.split("").reverse().join("");
+  if (s === k){
+    return true;
+  };
+  return false;
+};
+
+// test case:
+s = "race a car";
+isPalindrome(s);
