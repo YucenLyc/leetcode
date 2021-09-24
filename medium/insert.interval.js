1203ca48fe94eb58d95dc889cd3685de2a45cea6
YucenLyc <yucen.u@gmail.com>
2021-09-23 18:06:20 -0700

    solve

--- medium/insert.interval.js
+++ medium/insert.interval.js
@@ -3,10 +3,32 @@
 // Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
 
 const insert = function (intervals, newInterval) {
+  let result = [];
 
+  for (let i = 0; i < intervals.length; i++) {
+    let interval = intervals[i];
+
+    // if overlaps: 
+    if (Math.max(interval[0], newInterval[0]) <= Math.min(interval[1], newInterval[1])) {
+      newInterval = [Math.min(interval[0], newInterval[0]), Math.max(interval[1], newInterval[1])];
+      continue;
+    }
+
+    // if newInterval is lower:
+    if (interval[0] > newInterval[1]) {
+      result.push(newInterval, ...intervals.slice(i));
+      return result;
+    }
+
+    result.push(interval);
+  }
+  result.push(newInterval);
+  return result;
 };
 
+
 // test case:
-intervals = [[1,3], [6,9]]
-newInterval = [2,5]
-insert(intervals,newInterval);
+intervals = [[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]]
+newInterval = [4, 8]  // [[1,2], [3,10], [12,16]]
+insert(intervals, newInterval);
+
