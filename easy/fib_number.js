e3845d7ce6ca2cf7d8dfaffcf5e0b82acfb79488
YucenLyc <yucen.u@gmail.com>
2021-10-12 12:34:56 -0700

    memoization solution

--- easy/fib_number.js
+++ easy/fib_number.js
@@ -3,24 +3,34 @@
 // F(0) = 0, F(1) = 1
 // F(n) = F(n-1) + F(n-2), for n > 1
 
-const fib = function(n) {
-  if (n === 0) return 0;
-  if (n === 1) return 1; 
+// const fib = function(n) {
+//   if (n === 0) return 0;
+//   if (n === 1) return 1; 
 
-  let a = 0;
-  let b = 1;
-  let sum = a + b;
+//   let a = 0;
+//   let b = 1;
+//   let sum = a + b;
 
-  while (n > 1) {
-    sum = a + b;
-    a = b;
-    b = sum;
+//   while (n > 1) {
+//     sum = a + b;
+//     a = b;
+//     b = sum;
 
-    n--;
-  }
-  return sum;
-};
+//     n--;
+//   }
+//   console.log(sum)
+//   return sum;
+// };
+
+// MEMORIZATION:
+// create a js object, keys will be arg to fn, value will be the return value
+const fib = (n, memo = {}) => {
+  if (n in memo) return memo[n];
+  if (n <= 2) return 1;
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  return memo[n];
+}
 
 // test case: 
-n = 5;
-fib(n);
+n = 13;
+console.log(fib(n));
