f8c5af7f210fd6a9255f7e57071cecf78dc54bde
YucenLyc <yucen.u@gmail.com>
2021-10-14 17:24:57 -0700

    memoization solution

--- dp/howSum.js
+++ dp/howSum.js
@@ -1,21 +1,39 @@
 // write a function 'howSum(targetSum, numbers)' that takes in a target sum and an array of numbers as arguments.  
 
 // brute force solution:
-const howSum = function(targetSum, numbers) {
-  if (targetSum === 0) return [];
-  if (targetSum < 0 ) return null; // if the remainder goes negative 
+// const howSum = function(targetSum, numbers) {
+//   if (targetSum === 0) return [];
+//   if (targetSum < 0 ) return null; // if the remainder goes negative 
 
-  for (let num of numbers){
+//   for (let num of numbers){
+//     const remainder = targetSum - num;
+//     const remainderResult = howSum(remainder, numbers);
+//     if(remainderResult !== null) {
+//       return[ ...remainderResult, num]; //spread operator
+//     }
+//   }
+//   return null;
+// };
+
+// memoization solution: 
+const howSum = function (targetSum, numbers, memo = {}) {
+  if (targetSum in memo) return [];
+  if (targetSum === 0) return [];
+  if (targetSum < 0) return null; // if the remainder goes negative 
+
+  for (let num of numbers) {
     const remainder = targetSum - num;
-    const remainderResult = howSum(remainder, numbers);
-    if(remainderResult !== null) {
-      return[ ...remainderResult, num]; //spread operator
+    const remainderResult = howSum(remainder, numbers, memo);
+    if (remainderResult !== null) {
+      memo[targetSum] = [...remainderResult, num]; //spread operator
+      return memo[targetSum];
     }
   }
-  return null;
+  memo[targetSum] = null;
+  return memo[targetSum]
 };
 
 // test case: 
-targetSum = 8; 
-numbers = [2,3,5];
+targetSum = 30;
+numbers = [2, 3, 5];
 console.log(howSum(targetSum, numbers));
